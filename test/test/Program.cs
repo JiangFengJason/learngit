a890ab1fa88e04f0f44907fddb6ecfd0cb1143dc
JF <15828589290@163.com>
2016-10-26 21:46:29 +0800

    first commit

---
+++ test/test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<string> list = new Solution_1().FizzBuzz(15);
+            foreach(String s in list){
+                Console.WriteLine(s);
+            }
+        }
+        
+        
+    }
+}
---
+++ test/test/Solution_1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Solution_1
+    {
+        public IList<string> FizzBuzz(int n)
+        {
+            IList<string> mlist = new List<string>();
+            for (int i = 1; i <= n; ++i)
+            {
+                if (i % 3 == 0 && i % 5 != 0)
+                {
+                    mlist.Add("Fizz");
+                }
+                else if (i % 5 == 0 && i % 3 != 0)
+                {
+                    mlist.Add("Buzz");
+                }
+                else if (i % 3 == 0 && i % 5 == 0)
+                {
+                    mlist.Add("FizzBuzz");
+                }
+                else
+                {
+                    mlist.Add(i.ToString());
+                }
+            }
+            return mlist;
+        }
+    }
+}
